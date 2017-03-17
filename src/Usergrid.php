2191af46fe8694b1a20de4af9cb531fd54a4bb83
yousif aziz <yousif aziz>
2017-03-17 22:01:27 +0200

    init

---
+++ src/Usergrid.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Joeaziz\Usergrid;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserGrid
+ *
+ * @author jooAziz
+ */
+class Usergrid {
+
+    public function start() {
+        echo "it`s working";
+    }
+
+}
